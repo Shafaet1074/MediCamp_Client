1f35e31b859f18aa219432a3afa6ce10bb416ce2
shafaet.hossain@g.bracu.ac.bd <shafaet.hossain@g.bracu.ac.bd>
2024-06-05 19:43:43 +0600

    environment variable for firebase setup done

--- src/Components/Providers/firebase.config.js
+++ src/Components/Providers/firebase.config.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDVTcI5ey_1WIaYQfFyO32HQEkvqDjbeBI",
-  authDomain: "medicamp-70825.firebaseapp.com",
-  projectId: "medicamp-70825",
-  storageBucket: "medicamp-70825.appspot.com",
-  messagingSenderId: "33827499063",
-  appId: "1:33827499063:web:bf46d880eccae2163363b2"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain:import.meta.env.VITE_AUTHDOMAIN ,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_STORAGEBUCKET,
+  appId:import.meta.env.VITE_APPID  
 };
 
 // Initialize Firebase
